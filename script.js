ca2fbf14abefd45beebb98a8292f3a1f6fccb52d
Kamila Kryszak <kamilakryszak@yahoo.com>
2021-05-21 22:25:02 +0200

    feat: movements DOM manipulation

--- script.js
+++ script.js
@@ -61,6 +61,27 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach((mov, i) => {
+
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html =
+      `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+    <div class="movements__value">${mov}</div>
+  </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,41 +95,3 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // /////////////////////////////////////////////////
-
-
-const eurToUSD = 1.1;
-const movementsUSD = movements.map(mov => mov * eurToUSD);
-console.log(movementsUSD);
-
-
-// const dogsJulia = [3, 5, 2, 12, 7];
-// const dogsKate = [4, 1, 15, 8, 3];
-
-
-// const checkDogs = function (dogsJulia, dogsKate) {
-
-//   const juliaCopy = dogsJulia.slice();
-//   juliaCopy.splice(0, 1);
-//   juliaCopy.splice(-2);
-//   console.log(juliaCopy);
-
-//   const dogs = [...juliaCopy, ...dogsKate];
-//   console.log(dogs);
-
-//   dogs.forEach((dog, i) => {
-//     if (dog < 3) {
-//       console.log(`Dog number ${i+1} is a puppy`);
-//     } else {
-//       console.log(`Dog number ${i+1} is an adult`);
-//     }
-//   })
-
-// };
-
-// for (const animal of dogsKate) {
-//   console.log(animal);
-// }
-
-
-// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
